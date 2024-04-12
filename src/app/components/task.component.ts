0e8765e700fd4d4c139e372f8527007083e6ed55
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-12 16:26:57 +0200

    Improve styling of add new task

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -5,7 +5,7 @@ import {NgForOf} from "@angular/common";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
-import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatList, MatListItem} from "@angular/material/list";
 import {MatDividerModule} from "@angular/material/divider";
@@ -21,9 +21,21 @@ interface Task {
   template: `
     <mat-card>
       <mat-card-header>
-        <mat-card-title>Tasks</mat-card-title>
+        <mat-card-title>My Tasks</mat-card-title>
       </mat-card-header>
+
       <mat-card-content>
+        <form [formGroup]="taskForm" class="add-task-form">
+          <button (click)="addTask()" mat-mini-fab color="primary" aria-label="Add a task"
+                  style="margin: 40px 10px 0 17px">
+            <mat-icon>add</mat-icon>
+          </button>
+          <mat-form-field appearance="fill" style="width: 80%">
+            <mat-label>New Task</mat-label>
+            <input matInput formControlName="newTask" placeholder="Enter new task">
+          </mat-form-field>
+        </form>
+
         <mat-list *ngFor="let task of tasks">
           <mat-list-item>
             <mat-checkbox [ngModel]="task.checked" (change)="toggleChecked(task, $event)">
@@ -31,16 +43,9 @@ interface Task {
             </mat-checkbox>
           </mat-list-item>
         </mat-list>
-
-        <form [formGroup]="taskForm">
-          <mat-form-field appearance="fill">
-            <mat-label>New Task</mat-label>
-            <input matInput formControlName="newTask" placeholder="Enter new task">
-          </mat-form-field>
-          <button mat-raised-button color="primary" (click)="addTask()">Add Task</button>
-        </form>
       </mat-card-content>
     </mat-card>
+
   `,
   imports: [
     MatFormFieldModule, MatInputModule,
@@ -59,13 +64,13 @@ interface Task {
     MatListItem,
     MatList,
     MatIconButton,
-    MatDividerModule, FormsModule
+    MatDividerModule, FormsModule, MatMiniFabButton
   ],
   standalone: true
 })
 export class TaskComponent {
   tasks: Task[] = [
-    {name: 'Setup project with Angular', checked: false}, // All tasks added with checked: false
+    {name: 'Setup project with Angular', checked: false},
     {name: 'Add Material UI and icons', checked: false},
     {name: 'Publish project on Github', checked: false},
     {name: 'Setup toolbar and title', checked: false},
@@ -82,13 +87,12 @@ export class TaskComponent {
   addTask() {
     const newTaskName = this.taskForm.value.newTask;
     if (newTaskName) {
-      this.tasks.push({name: newTaskName, checked: false}); // New tasks added with checked: false
+      this.tasks.push({name: newTaskName, checked: false});
       this.taskForm.reset();
     }
   }
 
   toggleChecked(task: Task, event: MatCheckboxChange) {
-    // Directly delete the task on checkbox selection (checked)
     if (event.checked) {
       const index = this.tasks.indexOf(task);
       if (index > -1) {
