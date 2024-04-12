7f382c2c8f5245c42cce1fde09ff7860054335ee
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-12 16:01:14 +0200

    Add routing for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from "./components/home.component";
+import {AuthGuard} from "./services/auth-guard.service";
 import {LoginComponent} from "./components/login.component";
-import {isUserLoggedInGuard} from "./services/auth-guard.service";
+import {HomeComponent} from "./components/home.component";
+import {RegistrationComponent} from "./components/registration.component";
+import {ChallengeDetailComponent} from "./components/challenges/challenge-detail.component";
 
 export const routes: Routes = [
   {path: '', component: LoginComponent, pathMatch: 'full'},
-  {path: 'home', component: HomeComponent, canActivate: [isUserLoggedInGuard]},
-  {path: '**', redirectTo: '/home'}
+  {path: 'register', component: RegistrationComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'challenge-edit', component: ChallengeDetailComponent, canActivate: [AuthGuard]},
+  {path: '**', redirectTo: '/home'},
 ];
