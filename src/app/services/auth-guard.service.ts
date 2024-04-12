2d57ad3c3f3143e277fb038929395476033e3eb3
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-12 15:59:44 +0200

    Add guard for protecting routes

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,18 +1,17 @@
-import {inject} from "@angular/core";
-import {AuthService} from "./auth.service";
-import {Router} from "@angular/router";
+import {inject} from '@angular/core';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
+import {AuthUser} from "../shared/models";
+import {map} from "rxjs/operators";
 
-export const isUserLoggedInGuard = async () => {
+export const AuthGuard = async () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   // Wait for the isAuthenticated promise to resolve
-  const authenticated = await authService.isAuthenticated();
+  const authenticated = authService.user.pipe(map((user: AuthUser | null) => {
+    return user !== null;
+  }));
 
-  if (authenticated) {
-    return true;
-  } else {
-    await router.navigate(['/']);
-    return false;
-  }
+  return authenticated || (await router.navigate(['/']));
 };
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,1 @@
+export const ERROR_MESSAGE = 'You must enter a valid value';
