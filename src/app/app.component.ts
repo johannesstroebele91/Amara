4d6a4da9429e3cc8ce76a76b6ea14c5ce0fd00d5
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-12 15:58:26 +0200

    Setup basic home page and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,62 @@
-import {Component} from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCheckbox} from "@angular/material/checkbox";
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {TimerComponent} from "./components/timer.component";
-import {TaskComponent} from "./components/task.component";
-import {LoginComponent} from "./components/login.component";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {MatNativeDateModule} from "@angular/material/core";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCalendar} from "@angular/material/datepicker";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
 import {NgIf} from "@angular/common";
+import {Subscription} from "rxjs";
 import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatToolbarModule, MatCheckbox, MatCardModule, MatButtonModule, TimerComponent, TaskComponent, LoginComponent, RouterLink, NgIf],
+  imports: [MatToolbarModule, MatNativeDateModule, RouterOutlet, MatButton, MatCalendar, MatIcon, MatMenuTrigger, MatIconButton, MatMenu, MatCard, MatCardTitle, MatCardHeader, MatCardContent, MatToolbar, RouterLink, NgIf],
   template: `
     <mat-toolbar color="primary">
       <mat-toolbar-row>
-        <span>{{ title }}</span>
+        <span style="letter-spacing: 1px">{{ title }}</span>
+        <mat-icon style="margin-left: 5px">alarm</mat-icon>
         <span style="flex: 1 1 auto;"></span>
-        <a *ngIf="isNotHomeRoute()" routerLink="/" (click)="logout()"
-           style="color: white; text-underline: none;">Logout</a>
+        <a *ngIf="isNotOnLoginOrRegisterPage() && isAuthenticated" routerLink="/" (click)="logout()"
+           style="color: white; text-decoration: none; font-size: 16px">Logout</a>
       </mat-toolbar-row>
     </mat-toolbar>
-    <div style="max-width: 600px; margin: 30px auto;">
+    <div style=" margin: 60px auto;">
       <router-outlet></router-outlet>
     </div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
   title = 'Amara';
+  private userSub: Subscription | undefined;
 
   constructor(private router: Router, private authService: AuthService) {
   }
 
-  isNotHomeRoute(): boolean {
-    return this.router.url !== '/';
+  ngOnInit(): void {
+    this.authService.autoLoginAfterReload();
+
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+    })
+  }
+
+  isNotOnLoginOrRegisterPage(): boolean {
+    const isOnLoginPage = this.router.url !== '/';
+    const isOnRegisterPage = this.router.url !== '/register';
+    return (isOnLoginPage && isOnRegisterPage);
   }
 
   logout() {
     this.authService.logout()
+  }
+
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe()
   }
 }
