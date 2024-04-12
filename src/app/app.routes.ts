0461ebb73a4da99a52da90b55eda95cac881735f
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-12 16:12:51 +0200

    Remove over-complex challenge idea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import {AuthGuard} from "./services/auth-guard.service";
 import {LoginComponent} from "./components/login.component";
 import {HomeComponent} from "./components/home.component";
 import {RegistrationComponent} from "./components/registration.component";
-import {ChallengeDetailComponent} from "./components/challenges/challenge-detail.component";
 
 export const routes: Routes = [
   {path: '', component: LoginComponent, pathMatch: 'full'},
   {path: 'register', component: RegistrationComponent},
   {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'challenge-edit', component: ChallengeDetailComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: '/home'},
 ];
